0ebcf1b3473a2da3602f604cb79632c8fcd012da
Lykos20 <38576448+Lykos20@users.noreply.github.com>
2021-12-08 02:33:26 -0800

    Add files via upload

---
+++ docs/dir_47ef064b32b6557f71e7d7fa37484f93.js
@@ -0,0 +1,4 @@
+var dir_47ef064b32b6557f71e7d7fa37484f93 =
+[
+    [ "src", "dir_b1ae1e76b161d7e7d7698babf2575bb7.html", "dir_b1ae1e76b161d7e7d7698babf2575bb7" ]
+];
---
+++ docs/dir_b1ae1e76b161d7e7d7698babf2575bb7.js
@@ -0,0 +1,15 @@
+var dir_b1ae1e76b161d7e7d7698babf2575bb7 =
+[
+    [ "complex.h", "complex_8h_source.html", null ],
+    [ "main.cpp", "main_8cpp.html", "main_8cpp" ],
+    [ "shares.h", "shares_8h.html", "shares_8h" ],
+    [ "task_fft.h", "task__fft_8h_source.html", null ],
+    [ "task_get_data.cpp", "task__get__data_8cpp.html", "task__get__data_8cpp" ],
+    [ "task_get_data.h", "task__get__data_8h.html", "task__get__data_8h" ],
+    [ "task_motor1.cpp", "task__motor1_8cpp.html", "task__motor1_8cpp" ],
+    [ "task_motor1.h", "task__motor1_8h.html", "task__motor1_8h" ],
+    [ "task_motor2.cpp", "task__motor2_8cpp.html", "task__motor2_8cpp" ],
+    [ "task_motor2.h", "task__motor2_8h.html", "task__motor2_8h" ],
+    [ "task_put_in_array.cpp", "task__put__in__array_8cpp.html", "task__put__in__array_8cpp" ],
+    [ "task_put_in_array.h", "task__put__in__array_8h.html", "task__put__in__array_8h" ]
+];
